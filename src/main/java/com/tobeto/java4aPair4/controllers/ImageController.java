a61b8458652da6fa31ca1c66b79fac15b302312e
omerfarukbala <omerfarukbala@gmail.com>
2024-03-23 23:41:05 +0300

    image repository

---
+++ src/main/java/com/tobeto/java4aPair4/controllers/ImageController.java
@@ -0,0 +1,34 @@
+package com.tobeto.java4aPair4.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tobeto.java4aPair4.entities.Image;
+import com.tobeto.java4aPair4.repositories.ImageRepository;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageController {
+	private ImageRepository imageRepository;
+
+	public ImageController(ImageRepository imageRepository) {
+		super();
+		this.imageRepository = imageRepository;
+	}
+	
+	@GetMapping
+	public List<Image> getAll(){
+		return imageRepository.findAll();
+	}
+	
+	@PostMapping
+	public String add(@RequestBody Image image) {
+		imageRepository.save(image);
+		return "Resim başarıyla eklendi";
+	}
+}
