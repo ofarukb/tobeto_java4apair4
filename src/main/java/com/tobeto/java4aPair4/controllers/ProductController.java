4c3c44bb7c46df8660bbb5ae68398290567ba248
omerfarukbala <omerfarukbala@gmail.com>
2024-03-25 14:05:48 +0300

    Product'a http put,delete eklendi

--- src/main/java/com/tobeto/java4aPair4/controllers/ProductController.java
+++ src/main/java/com/tobeto/java4aPair4/controllers/ProductController.java
@@ -2,10 +2,13 @@ package com.tobeto.java4aPair4.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tobeto.java4aPair4.entities.Product;
@@ -23,12 +26,25 @@ public class ProductController {
 
 	@GetMapping
 	public List<Product> getAll() {
-		List<Product> products=productRepository.findAll();
+		List<Product> products = productRepository.findAll();
 		return products;
 	}
 
 	@PostMapping
 	public String add(@RequestBody Product product) {
+		productRepository.save(product);
+		return "Ürün başarıyla Eklendi.";
+	}
+
+	@PutMapping
+	public String update(@RequestBody Product product) {
+		productRepository.save(product);
+		return "Ürün başarıyla günncellendi.";
+	}
+
+	@DeleteMapping
+	public String add(@RequestParam int id) {
+		Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Ürün bulunamadı"));
 		productRepository.save(product);
 		return "Ürün başarıyla Eklendi.";
 	}
