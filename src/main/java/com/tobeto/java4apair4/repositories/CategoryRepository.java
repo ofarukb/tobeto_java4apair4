21f9b4550ff556540cde6bb5658e8289f03e4ba0
omerfarukbala <omerfarukbala@gmail.com>
2024-04-05 03:11:33 +0300

    custom query for category

--- src/main/java/com/tobeto/java4apair4/repositories/CategoryRepository.java
+++ src/main/java/com/tobeto/java4apair4/repositories/CategoryRepository.java
@@ -1,13 +1,18 @@
 package com.tobeto.java4apair4.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tobeto.java4apair4.entities.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 	Optional<Category> findByName(String categoryName);
+	
+	@Query("SELECT c FROM Category c ORDER BY id")
+	List<Category> findAllOrderByIdAsc();
 
 }
